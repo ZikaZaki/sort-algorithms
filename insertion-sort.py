7d33d4f464c470436cc948d239863a549672bae0
Zakariya Al-Khamisi <17160061@su.edu.ye>
2024-08-25 00:43:12 +0300

    Update insertion-sort.py

--- insertion-sort.py
+++ insertion-sort.py
@@ -1,1 +1,27 @@
+# Swap helper method to swap array elements
+def swap(arr, i, j):
+    arr[i], arr[j] = arr[j], arr[i]
+    return arr
 
+# insertion_sort from start-to-end of array
+def insertion_sort(arr):
+    for i in range(1, len(arr)):
+        j = i
+        while j > 0:
+            if (arr[j-1] > arr[j]):
+                print(f"Compared: ({arr[j-1]}, {arr[j]})")
+                swap(arr, j-1, j)
+                print(f"Swapped: {arr}")
+            else:
+                break
+            # decrement j to compare (j-1) with previous values till (0)
+            j -= 1
+    # print final sorted array
+    print(f"Final Sorted: {arr}")
+    return arr
+
+
+
+my_list = [3,0,1,8,7,2,5,4,9,6]
+
+insertion_sort(my_list)
