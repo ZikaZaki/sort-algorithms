06b7f6359943d289b32ee9810959113ea219f34a
Zakariya Al-Khamisi <17160061@su.edu.ye>
2024-08-31 04:44:48 +0300

    update reversed_insertion_sort method with correct implementation

--- insertion-sort.py
+++ insertion-sort.py
@@ -15,21 +15,23 @@ def insertion_sort(array):
     return array
     
 
-# reversed_insertion_sort: from end-to-start of array
-def reversed_insertion_sort(arr):
-    for i in range(len(arr) - 1, 0, -1):
+# reversed_insertion_sort
+def reversed_insertion_sort(array):
+    for i in range(len(array)-1, -1, -1):
+        key = array[i]
         j = i
-        while j < len(arr) and arr[j-1] > arr[j]:
-            print(f"Compared: ({arr[j-1]}, {arr[j]})")
-            swap(arr, j-1, j)
-            print(f"Swapped: {arr}")
+
+        while j+1 < len(array) and array[j+1] < key:
+            array[j] = array[j+1]
             j += 1
 
-    # Print final sorted array
-    print(f"Final Sorted: {arr}")
-    return arr
+        # place the (key) in its correct position
+        array[j] = key
 
-my_list = [3,0,1,8,7,2,5,4,9,6]
+    # return final sorted array
+    return array
 
-insertion_sort(my_list)
-reversed_insertion_sort(my_list)
+arr = [3, 0, 1, 8, 7, 2, 5, 4, 9, 6]
+
+print(insertion_sort(arr))
+print(reversed_insertion_sort(arr))
