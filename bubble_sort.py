75115af89ade440c82bc8654b238ee54931f8f38
Zakariya Al-Khamisi <17160061@su.edu.ye>
2024-09-01 00:40:16 +0300

    add bubble_sort method implementation

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,1 +1,15 @@
+def bubble_sort(array):
+  for i in range(len(array)):
+    has_swaps = False
+    for j in range(0, len(array)-i-1):
+      if(array[j] > array[j+1]):
+        array[j], array[j+1] = array[j+1], array[j]
+        has_swaps = True
 
+    # check if there's no swap to early exit
+    if(not has_swaps):
+      break
+
+arr = [9,53,5,0,2,3,1,4,9]
+bubble_sort(arr)
+print(arr)
