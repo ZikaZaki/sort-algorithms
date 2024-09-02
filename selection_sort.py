adf477af8e7460f4d1cf9cf09bff3973bb906b34
Zakariya Al-Khamisi <17160061@su.edu.ye>
2024-09-03 02:04:03 +0300

    implement the selection_sort method

--- selection_sort.py
+++ selection_sort.py
@@ -1,1 +1,14 @@
+def selection_sort(array):
+  for i in range(len(array)):
+    min_index = i
+    for j in range(i+1, len(array)):
+      if array[j] < array[min_index]:
+        min_index = j
+
+    array[i], array[min_index] = array[min_index], array[i]
+
+
+arr = [5,4,7,2,15,16,11,1]
+selection_sort(arr)
+print("Sorted Array: ", arr)
 
