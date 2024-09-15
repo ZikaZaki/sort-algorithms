73b454d308545a5ac4ea7bab5bd8a90773012da2
Zakariya Al-Khamisi <17160061@su.edu.ye>
2024-09-15 23:55:45 +0300

    Create quick_sort.js

---
+++ quick_sort.js
@@ -0,0 +1,39 @@
+// Quick Sort iterative implementation
+function partition(arr, low, high){
+    const pivot = arr[high];
+    let i = low - 1;
+    
+    for(let j = low; j < high; j++){
+        if(arr[j] <= pivot){
+            i += 1;
+            if (i === j) continue; // to skip unnecessary swaps
+            [arr[i], arr[j]] = [arr[j], arr[i]]
+        }
+    }
+    
+    [arr[i+1], arr[high]] = [arr[high], arr[i+1]];
+    return i+1;
+}
+
+
+function quickSort(arr){
+    const stack = [[0, arr.length - 1]];
+    
+    while (stack.length > 0){
+        const [low, high] = stack.pop();
+        
+        if(low < high){
+            const pivotIndex = partition(arr, low, high);
+
+            // left side
+            stack.push([low, pivotIndex - 1]);
+            // right side
+            stack.push([pivotIndex + 1, high]);
+        }
+    }
+    
+    return arr;
+}
+
+const arr = [53,9,5,9,2,3,1,4,0];
+console.log("sorted: ", quickSort(arr));
