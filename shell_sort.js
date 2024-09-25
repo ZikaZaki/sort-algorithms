96c810b56f663aca9fd1239c52ded7a176572f08
Zakariya Al-Khamisi <17160061@su.edu.ye>
2024-09-26 01:18:33 +0300

    Create shell_sort.js

---
+++ shell_sort.js
@@ -0,0 +1,25 @@
+function shellSort(arr){
+    const n = arr.length;
+    let gap = Math.floor(n / 2);
+    
+    while (gap > 0){
+        for (let i = gap; i < n; i++){
+            let temp = arr[i];
+            let j = i;
+            
+            while (j >= gap && arr[j-gap] > temp){
+                arr[j] = arr[j-gap];
+                j -= gap;
+            }
+            // replace j with temp
+            arr[j] = temp;
+        }
+        
+        gap = Math.floor(gap / 2);
+    }
+    
+    return arr;
+}
+
+let arr = [56,23,6,2,0,9,1,0,32];
+console.log("Sorted: ", shellSort(arr));
