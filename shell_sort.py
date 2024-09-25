6d81fd7542886be7101a3e0450df159f88cda9c6
Zakariya Al-Khamisi <17160061@su.edu.ye>
2024-09-26 01:14:43 +0300

    add shell sort implementation

---
+++ shell_sort.py
@@ -0,0 +1,24 @@
+def shell_sort(arr):
+  n = len(arr)
+  gap = n // 2
+  
+  while gap > 0:
+    for i in range(gap, n):
+      temp = arr[i]
+      j = i
+
+      while j >= gap and arr[j-gap] > temp:
+        arr[j] = arr[j-gap]
+        j -= gap
+
+      arr[j] = temp
+
+    gap //= 2
+
+  return arr
+
+
+  # Example usage
+arr = [0, 0, 1, 2, 6, 9, 32, 23, 56]
+print("Sorted array:", shell_sort(arr))
+    
