5ae787ed2a1108f476a0d7e7d361d2d400d36daf
Zakariya Al-Khamisi <17160061@su.edu.ye>
2024-09-28 00:30:56 +0300

    Update bucket_sort.js

--- bucket_sort.js
+++ bucket_sort.js
@@ -3,7 +3,7 @@ function bucketSort(arr, numOfBuckets=10){
   
   // Step 1: Create buckets based on numOfBuckets argument
   const buckets = Array.from({ length: numOfBuckets }, () => []);
-
+  
   // Step 2: Define factors used to distribute elements into the buckets
   const min = Math.min(...arr);
   const max = Math.max(...arr);
@@ -12,12 +12,19 @@ function bucketSort(arr, numOfBuckets=10){
   // Avoid division by zero if all elements are equal
   if(range === 0) return arr;
   
+  // Step 3: Calculate the bucket index for each element and insert element in its calculated bucket  
   arr.forEach(element => {
-    let index = Math.floor((element - min) / range * (max -1));
+    let index = Math.floor((element - min) / range * (numOfBuckets -1));
+    index = Math.min(index, numOfBuckets - 1) // Prevents index out of range
     buckets[index].push(element);
   });
-
+  
+  // Step 4: Sort each bucket and concatenate the results. 
   arr = buckets.flatMap(bucket => bucket.sort((a, b) => a - b));
 
   return arr;
 }
+
+const arr = [6, 56, 1, 32, 0, 9, 2, 23, 0]
+
+console.log("sorted:", bucketSort(arr));
