bb0c1829ba14e176adbdac099e3a17a17b04fd74
Zakariya Al-Khamisi <17160061@su.edu.ye>
2024-09-28 00:22:26 +0300

    Update bucket_sort.js

--- bucket_sort.js
+++ bucket_sort.js
@@ -9,7 +9,15 @@ function bucketSort(arr, numOfBuckets=10){
   const max = Math.max(...arr);
   const range = max - min;
 
-  for
+  // Avoid division by zero if all elements are equal
+  if(range === 0) return arr;
   
-  
+  arr.forEach(element => {
+    let index = Math.floor((element - min) / range * (max -1));
+    buckets[index].push(element);
+  });
+
+  arr = buckets.flatMap(bucket => bucket.sort((a, b) => a - b));
+
+  return arr;
 }
