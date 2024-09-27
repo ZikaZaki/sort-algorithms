d7f17e76b255b58ce3767136e312808edcf8bd78
Zakariya Al-Khamisi <17160061@su.edu.ye>
2024-09-28 00:09:49 +0300

    Update bucket_sort.js

--- bucket_sort.js
+++ bucket_sort.js
@@ -1,11 +1,15 @@
 function bucketSort(arr, numOfBuckets=10){
   if(arr.length === 0) return arr;
-
+  
+  // Step 1: Create buckets based on numOfBuckets argument
   const buckets = Array.from({ length: numOfBuckets }, () => []);
 
+  // Step 2: Define factors used to distribute elements into the buckets
   const min = Math.min(...arr);
   const max = Math.max(...arr);
   const range = max - min;
+
+  for
   
   
 }
